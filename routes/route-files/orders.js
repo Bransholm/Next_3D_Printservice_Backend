ba640d8b1b5419eda4c167b115a8229367af9123
Bransholm <kenneth@bransholm.dk>
2024-01-28 18:18:04 +0100

    ordersRouter put with id for change of status

--- routes/route-files/orders.js
+++ routes/route-files/orders.js
@@ -22,4 +22,41 @@ ordersRouter.get("/", async (request, response) => {
   }
 });
 
+
+
+// Put route for oders by ID on Status
+async function updateVariablesQuery(id, request) {
+  const body = request.body;
+
+  const queryString =
+    /*sql*/
+    `
+        UPDATE orders SET Status=? WHERE Id =?;
+    `;
+
+  const updateValues = [
+      body.status,
+      id,
+  ];
+  const [result] = await dbConnection.query(queryString, updateValues);
+  return result;
+}
+ordersRouter.put("/:id", async (request, response) => {
+  const id = request.params.id;
+  try {
+    const result = await updateVariablesQuery(id, request);
+    if (!result || result.affectedRows === 0) {
+      rowIdNotFoundResponse(id, response);
+    } else {
+      response.json(result);
+    }
+  } catch (error) {
+    console.error(error);
+    response
+      .status(500)
+      .json({ message: "An Internal Server Error Has Occured" });
+  }
+});
+
+
 export default ordersRouter;
