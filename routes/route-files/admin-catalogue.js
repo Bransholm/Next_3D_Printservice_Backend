f30163aecf65c9e79b07d96075eb7d937cd2bbb9
Bransholm <kenneth@bransholm.dk>
2024-01-28 16:25:03 +0100

    Copy of old route removed

--- routes/route-files/admin-catalogue.js
+++ routes/route-files/admin-catalogue.js
@@ -26,25 +26,4 @@ adminCatalogueRouter.get("/", async (request, response) => {
   }
 });
 
-
-/// KOPI
-// adminCatalogueRouter.get("/", async (request, response) => {
-//   try {
-//     const active = request.query.active;
-//     const queryString =
-//       /*sql*/
-//       `
-//         SELECT * FROM Catalogue
-//         WHERE Active = '${active}';
-//     `;
-//     const [result] = await dbConnection.execute(queryString);
-//     response.json(result);
-//   } catch (error) {
-//     console.error(error);
-//     response
-//       .status(500)
-//       .json({ message: "An Internal Server Error Has Occured" });
-//   }
-// });
-
 export default adminCatalogueRouter;
